acd4b6cff744a4d9009b9e6c6103881f557c3f4f
Ian Suaznabar Veliz <ianveliz19@gmail.com>
2022-09-07 10:48:11 -0400

    Add Enviroment Vars

---
+++ app.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+
+userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+
+const User = new mongoose.model("User",userSchema);
+
+app.get("/",function(req,resp){
+  resp.render("home");
+});
+
+app.get("/register",function(req,resp){
+  resp.render("register");
+});
+
+app.get("/login",function(req,resp){
+  resp.render("login");
+});
+
+app.post("/register",function(req,resp){
+  const newUser = User ({
+    email:req.body.username,
+    password:req.body.password
+  });
+
+  newUser.save(function(err){
+    if(err){
+    console.log(err);
+    }else{
+      resp.render("secrets");
+    }
+  });
+});
+
+app.post("/login",function(req,resp){
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          resp.render("secrets")
+        }
+      }
+    }
+  });
+});
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
