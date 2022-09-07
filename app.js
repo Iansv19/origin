2ff122dbe5d851c46e25aae9cf933e417f78a5ac
Ian Suaznabar Veliz <ianveliz19@gmail.com>
2022-09-07 11:07:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
